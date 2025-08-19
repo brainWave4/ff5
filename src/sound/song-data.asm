372658629bede45e231a1415197ff3be1ad10a1b
mechPenSketch <pikadon92+1@gmail.com>
2025-08-20 01:26:02 +0800

    Comment Instruments

--- src/sound/song-data.asm
+++ src/sound/song-data.asm
@@ -21,121 +21,121 @@ SampleBRRPtrs:
 
 ; c4/3cd8
 SampleLoopStart:
-        .word   $0a8c
-        .word   $0bd9
-        .word   $1194
-        .word   $05fa
-        .word   $15f9
-        .word   $0465
-        .word   $1194
-        .word   $1194
-        .word   $04f5
-        .word   $029a
-        .word   $08f7
-        .word   $02c7
-        .word   $034e
-        .word   $04da
-        .word   $0252
-        .word   $0489
-        .word   $0936
-        .word   $0642
-        .word   $0144
-        .word   $044a
-        .word   $05bb
-        .word   $0666
-        .word   $1194
-        .word   $09bd
-        .word   $0384
-        .word   $05bb
-        .word   $092d
-        .word   $15f9
-        .word   $0318
-        .word   $077d
-        .word   $08dc
-        .word   $088d
-        .word   $09e1
-        .word   $0e7c
-        .word   $0b6d
+        .word   $0a8c           ; BASS_DRUM
+        .word   $0bd9           ; SNARE
+        .word   $1194           ; HARD_SNARE
+        .word   $05fa           ; CYMBAL
+        .word   $15f9           ; TOM
+        .word   $0465           ; CLOSED_HIHAT
+        .word   $1194           ; OPEN_HIHAT
+        .word   $1194           ; TIMPANI
+        .word   $04f5           ; VIBRAPHONE
+        .word   $029a           ; MARIMBA
+        .word   $08f7           ; STRINGS
+        .word   $02c7           ; CHOIR
+        .word   $034e           ; HARP
+        .word   $04da           ; TRUMPET
+        .word   $0252           ; OBOE
+        .word   $0489           ; FLUTE
+        .word   $0936           ; ORGAN
+        .word   $0642           ; PIANO
+        .word   $0144           ; ELECTRIC_BASS
+        .word   $044a           ; BASS_GUITAR
+        .word   $05bb           ; GRAND_PIANO
+        .word   $0666           ; MUSIC_BOX_INSTR
+        .word   $1194           ; WOO
+        .word   $09bd           ; METAL_SYSTEM
+        .word   $0384           ; SYNTH_CHORD
+        .word   $05bb           ; DIST_GUITAR
+        .word   $092d           ; KRABI
+        .word   $15f9           ; HORN
+        .word   $0318           ; MANDOLIN
+        .word   $077d           ; UNKNOWN_1
+        .word   $08dc           ; CONGA
+        .word   $088d           ; CASABA
+        .word   $09e1           ; KLAVES
+        .word   $0e7c           ; UNKNOWN_2
+        .word   $0b6d           ; HAND_CLAP
 
 ; ---------------------------------------------------------------------------
 
 ; c4/3d1e
 SampleFreqMult:
-        .byte   $c0,$00
-        .byte   $00,$00
-        .byte   $c0,$00
-        .byte   $00,$00
-        .byte   $60,$00
-        .byte   $00,$00
-        .byte   $00,$00
-        .byte   $fb,$00
-        .byte   $fe,$48
-        .byte   $e0,$a0
-        .byte   $00,$7c
-        .byte   $fd,$00
-        .byte   $51,$00
-        .byte   $fe,$00
-        .byte   $e0,$90
-        .byte   $fc,$60
-        .byte   $fc,$7f
-        .byte   $ff,$00
-        .byte   $fc,$c0
-        .byte   $fc,$a0
-        .byte   $fc,$d0
-        .byte   $ff,$a0
-        .byte   $00,$00
-        .byte   $00,$00
-        .byte   $00,$00
-        .byte   $00,$00
-        .byte   $fe,$00
-        .byte   $e0,$b0
-        .byte   $fc,$90
-        .byte   $e0,$c0
-        .byte   $00,$00
-        .byte   $00,$00
-        .byte   $0e,$00
-        .byte   $00,$00
-        .byte   $00,$00
+        .byte   $c0,$00         ; BASS_DRUM
+        .byte   $00,$00         ; SNARE
+        .byte   $c0,$00         ; HARD_SNARE
+        .byte   $00,$00         ; CYMBAL
+        .byte   $60,$00         ; TOM
+        .byte   $00,$00         ; CLOSED_HIHAT
+        .byte   $00,$00         ; OPEN_HIHAT
+        .byte   $fb,$00         ; TIMPANI
+        .byte   $fe,$48         ; VIBRAPHONE
+        .byte   $e0,$a0         ; MARIMBA
+        .byte   $00,$7c         ; STRINGS
+        .byte   $fd,$00         ; CHOIR
+        .byte   $51,$00         ; HARP
+        .byte   $fe,$00         ; TRUMPET
+        .byte   $e0,$90         ; OBOE
+        .byte   $fc,$60         ; FLUTE
+        .byte   $fc,$7f         ; ORGAN
+        .byte   $ff,$00         ; PIANO
+        .byte   $fc,$c0         ; ELECTRIC_BASS
+        .byte   $fc,$a0         ; BASS_GUITAR
+        .byte   $fc,$d0         ; GRAND_PIANO
+        .byte   $ff,$a0         ; MUSIC_BOX_INSTR
+        .byte   $00,$00         ; WOO
+        .byte   $00,$00         ; METAL_SYSTEM
+        .byte   $00,$00         ; SYNTH_CHORD
+        .byte   $00,$00         ; DIST_GUITAR
+        .byte   $fe,$00         ; KRABI
+        .byte   $e0,$b0         ; HORN
+        .byte   $fc,$90         ; MANDOLIN
+        .byte   $e0,$c0         ; UNKNOWN_1
+        .byte   $00,$00         ; CONGA
+        .byte   $00,$00         ; CASABA
+        .byte   $0e,$00         ; KLAVES
+        .byte   $00,$00         ; UNKNOWN_2
+        .byte   $00,$00         ; HAND_CLAP
 
 ; ---------------------------------------------------------------------------
 
 ; c4/3d64
 SampleADSR:
-        make_adsr 15,15,7,0
-        make_adsr 15,15,7,0
-        make_adsr 15,15,7,0
-        make_adsr 15,15,7,16
-        make_adsr 15,15,7,0
-        make_adsr 15,15,7,0
-        make_adsr 15,15,7,0
-        make_adsr 15,15,7,0
-        make_adsr 15,15,7,16
-        make_adsr 15,15,7,21
-        make_adsr 15,15,7,0
-        make_adsr 15,15,7,0
-        make_adsr 15,15,7,18
-        make_adsr 15,15,7,1
-        make_adsr 15,15,7,1
-        make_adsr 15,15,7,1
-        make_adsr 15,15,7,0
-        make_adsr 15,15,7,13
-        make_adsr 15,15,7,0
-        make_adsr 15,15,7,12
-        make_adsr 15,15,7,10
-        make_adsr 15,15,7,19
-        make_adsr 15,15,7,0
-        make_adsr 15,15,7,10
-        make_adsr 15,15,7,10
-        make_adsr 15,15,7,8
-        make_adsr 15,15,7,19
-        make_adsr 15,15,7,1
-        make_adsr 15,15,7,17
-        make_adsr 15,15,7,20
-        make_adsr 15,15,7,19
-        make_adsr 15,15,7,0
-        make_adsr 15,15,7,0
-        make_adsr 15,15,7,0
-        make_adsr 15,15,7,0
+        make_adsr 15,15,7,0    ; BASS_DRUM
+        make_adsr 15,15,7,0    ; SNARE
+        make_adsr 15,15,7,0    ; HARD_SNARE
+        make_adsr 15,15,7,16   ; CYMBAL
+        make_adsr 15,15,7,0    ; TOM
+        make_adsr 15,15,7,0    ; CLOSED_HIHAT
+        make_adsr 15,15,7,0    ; OPEN_HIHAT
+        make_adsr 15,15,7,0    ; TIMPANI
+        make_adsr 15,15,7,16   ; VIBRAPHONE
+        make_adsr 15,15,7,21   ; MARIMBA
+        make_adsr 15,15,7,0    ; STRINGS
+        make_adsr 15,15,7,0    ; CHOIR
+        make_adsr 15,15,7,18   ; HARP
+        make_adsr 15,15,7,1    ; TRUMPET
+        make_adsr 15,15,7,1    ; OBOE
+        make_adsr 15,15,7,1    ; FLUTE
+        make_adsr 15,15,7,0    ; ORGAN
+        make_adsr 15,15,7,13   ; PIANO
+        make_adsr 15,15,7,0    ; ELECTRIC_BASS
+        make_adsr 15,15,7,12   ; BASS_GUITAR
+        make_adsr 15,15,7,10   ; GRAND_PIANO
+        make_adsr 15,15,7,19   ; MUSIC_BOX_INSTR
+        make_adsr 15,15,7,0    ; WOO
+        make_adsr 15,15,7,10   ; METAL_SYSTEM
+        make_adsr 15,15,7,10   ; SYNTH_CHORD
+        make_adsr 15,15,7,8    ; DIST_GUITAR
+        make_adsr 15,15,7,19   ; KRABI
+        make_adsr 15,15,7,1    ; HORN
+        make_adsr 15,15,7,17   ; MANDOLIN
+        make_adsr 15,15,7,20   ; UNKNOWN_1
+        make_adsr 15,15,7,19   ; CONGA
+        make_adsr 15,15,7,0    ; CASABA
+        make_adsr 15,15,7,0    ; KLAVES
+        make_adsr 15,15,7,0    ; UNKNOWN_2
+        make_adsr 15,15,7,0    ; HAND_CLAP
 
 ; ---------------------------------------------------------------------------
 
